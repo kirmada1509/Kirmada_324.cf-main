42ca6e3ddfcbbf33a5d378c751cdd8a7a8f9282c
kirmada150905 <174258997+kirmada150905@users.noreply.github.com>
2025-02-27 20:06:09 +0530

    long time no see

--- solns/prog.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-
-int main(){
-	string s = "abcd";
-	int res = 0;
-	for(char x: s)res += x;
-	cout<<res<<endl;
-}
--- solns/sliding.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<bits/stdc++.h>
-
-using namespace std
-
-int func(int n, int m, int r, int c){
-    
-}
